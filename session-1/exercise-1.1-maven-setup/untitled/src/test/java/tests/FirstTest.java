40a3e1c0a18d95342fd247a848d8a18d66f34cf1
Hoang Duy <hoangduy12052003@gmail.com>
2026-01-14 13:55:58 +0700

    feat: refactor test to use properties file for URL

--- session-1/exercise-1.1-maven-setup/untitled/src/test/java/tests/FirstTest.java
+++ session-1/exercise-1.1-maven-setup/untitled/src/test/java/tests/FirstTest.java
@@ -1,5 +1,7 @@
 package tests;
 
+import java.io.FileInputStream;
+import java.util.Properties;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -7,10 +9,19 @@ import org.testng.annotations.Test;
 
 public class FirstTest {
     @Test
-    public void openBrowserTest() {
+    public void openBrowserTest() throws Exception {
+
+        Properties props = new Properties();
+        FileInputStream fis = new FileInputStream("src/test/resources/config.properties");
+        props.load(fis);
+
+        String urlFromConfig = props.getProperty("url");
+
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
-        driver.get("https://google.com");
+
+        driver.get(urlFromConfig);
+
         driver.quit();
     }
 }
